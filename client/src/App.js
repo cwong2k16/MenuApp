41809762cc1952095e2a6990f98b770f4b6d6b56
cwong2015 <sirhc8002@gmail.com>
2018-01-27 00:40:47 -0500

    small changes to react boilerplate app to reflect restaurant's

--- client/src/App.js
+++ client/src/App.js
@@ -25,8 +25,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Precious Island Tea Shop Menu</h1>
         </header>
         <p className="App-intro">
           {this.state.response}
